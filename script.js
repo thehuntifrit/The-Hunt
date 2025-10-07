0603bbb0540d339296aefb1a132eb31e6c6da3db
thehuntifrit <the.hunt.ifrit@gmail.com>
2025-10-07 17:27:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Google Apps Script (GAS) のエンドポイントURL
+// ★新しいURLに変更しました★
 const GAS_ENDPOINT = 'https://script.google.com/macros/s/AKfycbwxgb5APRPyTwEM3ZQtgG3WWdxrFqVZAgkvq4Qfh_FggBU2p21yYDkWIdp-jMfBtG92Gg/exec';
 // 静的モブデータ (mob_data.json) のURL (同階層のファイルを参照)
 const MOB_DATA_URL = './mob_data.json'; 
