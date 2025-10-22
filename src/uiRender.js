27092b215ffad60ee307054bebf57d4455a44993
thehuntifrit <the.hunt.ifrit@gmail.com>
2025-10-22 13:24:25 +0900

    Update uiRender.js

--- src/uiRender.js
+++ src/uiRender.js
@@ -553,5 +553,4 @@ setInterval(() => {
 
 // ← この下に export をまとめる
 export { filterAndRender, distributeCards, updateProgressText, updateProgressBar, createMobCard, displayStatus, DOM,
-        renderAreaFilterPanel, renderRankTabs, sortAndRedistribute,  updateFilterUI, toggleAreaPanel, onKillReportReceived
-  };
+        renderAreaFilterPanel, renderRankTabs, sortAndRedistribute,  updateFilterUI, onKillReportReceived };
