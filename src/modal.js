0013db94efde099480c2b4d4e1acda5a289bc015
thehuntifrit <the.hunt.ifrit@gmail.com>
2025-10-29 08:55:32 +0900

    Update modal.js

--- src/modal.js
+++ src/modal.js
@@ -6,8 +6,7 @@ import { toJstAdjustedIsoString } from "./cal.js";
 
 function toLocalIsoString(date) { 
     const pad = n => String(n).padStart(2, "0"); 
-    return `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(date.getDate())}T${pad(date.getHours())}:${pad(date.getMinutes())
-    }`; 
+    return `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(date.getDate())}T${pad(date.getHours())}:${pad(date.getMinutes())}`; 
 }
 
 function openReportModal(mobNo) {
