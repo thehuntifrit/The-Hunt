719198dcb5b6e177e02c3cd03f3d0c9c840e83a3
kyle <the.hunt.ifrit@gmail.com>
2025-12-11 19:37:47 +0900

    Update app.js

--- src/app.js
+++ src/app.js
@@ -41,7 +41,7 @@ async function initializeApp() {
 
         setTimeout(() => {
             showColumnContainer();
-        }, 300);
+        }, 500);
 
     } catch (e) {
         console.error("App initialization failed:", e);
