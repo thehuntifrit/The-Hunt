c3228a21e9e4e82ae390f9a97ee7365ae24745d0
kyle <the.hunt.ifrit@gmail.com>
2026-01-09 08:55:22 +0900

    cal.js の更新

--- src/cal.js
+++ src/cal.js
@@ -521,7 +521,7 @@ function calculateRepop(mob, maintenance, options = {}) {
   } else {
     status = "PopWindow";
     elapsedPercent = Math.min(((now - minRepop) / (maxRepop - minRepop)) * 100, 100);
-    timeRemaining = `📶&thinsp;${formatDurationHM(maxRepop - now)}`;
+    timeRemaining = `⏳&thinsp;${formatDurationHM(maxRepop - now)}`;
   }
 
   if (isInConditionWindow && now >= minRepop) {
