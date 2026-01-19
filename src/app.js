bc60dbad3e3d632ace82723398d20c0e55d22398
kyle <the.hunt.ifrit@gmail.com>
2026-01-20 02:23:59 +0900

    Update app.js

--- src/app.js
+++ src/app.js
@@ -106,7 +106,7 @@ function renderMaintenanceStatus() {
 
         if (now >= showFrom && now <= showUntil) {
             maintenanceEl.innerHTML = `
-                <div class="font-semibold text-yellow-300">
+                <div class="font-semibold text-red-500">
                     メンテ日時 ${formatDate(start)} ～ ${formatDate(end)}
                 </div>
             `;
