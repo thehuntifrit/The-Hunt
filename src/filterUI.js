beebac370d8d2be2d3404e9df34c7bd28f5e4a93
kyle <the.hunt.ifrit@gmail.com>
2026-01-20 01:33:58 +0900

    Update filterUI.js

--- src/filterUI.js
+++ src/filterUI.js
@@ -136,9 +136,9 @@ const updateFilterUI = () => {
     const isCurrent = btnRank === state.filter.rank;
 
     btn.classList.remove(
-      "bg-rose-500", "bg-amber-500", "bg-indigo-500",
+      "bg-rose-700", "bg-amber-700", "bg-indigo-700",
       "bg-gray-500", "hover:bg-gray-400", "bg-green-500", "bg-gray-800",
-      "bg-rose-300", "bg-amber-300", "bg-indigo-300",
+      "bg-rose-500", "bg-amber-500", "bg-indigo-500",
       "bg-stone-500", "bg-rank-s", "bg-rank-a", "bg-rank-f"
     );
 
