d2258840507d229e309bcac6029326552f1c5aa5
kyle <the.hunt.ifrit@gmail.com>
2026-02-01 03:06:32 +0900

    Update filterUI.js

--- src/filterUI.js
+++ src/filterUI.js
@@ -29,7 +29,7 @@ export const renderRankTabs = () => {
     btn.textContent = rank;
 
     btn.className =
-      `tab-button px-2 py-1 text-sm rounded font-semibold text-white text-center transition ` +
+      `tab-button px-2 py-0.5 text-sm rounded font-semibold text-white text-center transition ` +
       (isSelected
         ? (rank === "ALL" ? "bg-rose-500" : rank === "S" ? "bg-rank-s" : rank === "A" ? "bg-rank-a" : rank === "FATE" ? "bg-rank-f" : "bg-green-500")
         : "bg-gray-500 hover:bg-gray-400");
@@ -71,7 +71,7 @@ export const renderAreaFilterPanel = () => {
     const btn = document.createElement("button");
     btn.textContent = label;
 
-    let btnClass = 'py-1 px-2 text-sm rounded font-semibold text-white text-center transition';
+    let btnClass = 'py-0.5 px-2 text-sm rounded font-semibold text-white text-center transition';
 
     if (uiRank === 'ALL' && !isAll) {
       if (isDesktop) btnClass += ' w-12';
